d0939e90d8ae2a3a716f645f5ff5b8c223ac750a
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-05-19 22:32:35 -0300

    install & config ESLint

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js, prettier },
+    extends: [
+      'js/recommended',
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+    ],
+  },
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    'env': {
+      'browser': true,
+      'es2021': true,
+    },
+  },
+]);
